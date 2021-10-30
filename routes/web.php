bb8bb24a74446fa66d8a49c6f744a636bb425031
nguyentuyen348 <nguyentuyen348@gmail.com>
2021-10-30 16:03:25 +0700

    update-profile-user

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 Route::get('/register',[RegisterController::class,'showFormRegister'])->name('register');
 Route::post('/register',[RegisterController::class,'register'])->name('auth.register');
 
