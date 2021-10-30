e96b054d847deb7f0cbc860cb8200714a302625d
khonghung <ahihimaianh3@gmail.com>
2021-10-30 20:33:39 +0700

    categories

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\CategoriesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,13 +16,18 @@ use App\Http\Controllers\CategoriesController;
 |
 */
 
+Route::get('/register', [RegisterController::class, 'showFormRegister'])->name('register');
+Route::post('/register', [RegisterController::class, 'register'])->name('auth.register');
+Route::get('/', [LoginController::class, 'showFormLogin'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.login');
+Route::get('/changePassword', [LoginController::class, 'showFormChangePassword'])->name('showChangePassword');
+Route::post('/changePassword', [LoginController::class, 'changePassword'])->name('changePassword');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
-  Route::get('/', [LoginController::class, 'showFormLogin'])->name('login');
-  Route::post('/login', [LoginController::class, 'login'])->name('login.login');
+Route::prefix('users')->group(function (){
+    Route::get('{id}/profile',[\App\Http\Controllers\UserController::class,'profile'])->name('users.profile');
+    Route::get('{id}/edit',[\App\Http\Controllers\UserController::class,'edit'])->name('users.edit');
+    Route::post('{id}/edit',[\App\Http\Controllers\UserController::class,'update'])->name('users.update');
+});
 
-  Route::get('/register',[RegisterController::class,'showFormRegister'])->name('register');
-  Route::post('/register',[RegisterController::class,'register'])->name('auth.register');
-
-  Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-
-  Route::get('/list', [CategoriesController::class, 'index'])->name('categories.index');
+Route::get('/list', [CategoriesController::class, 'index'])->name('categories.list');
