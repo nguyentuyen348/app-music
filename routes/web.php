0c32069807a92ba5498f81ec89d56bb9920f7369
khonghung <ahihimaianh3@gmail.com>
2021-10-30 14:25:16 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+  
+    Route::get('/', [LoginController::class, 'showFormLogin'])->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('login.login');
+
+
