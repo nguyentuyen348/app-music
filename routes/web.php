440525e882b0a7f2057bf4a83c116a54fd2b52b3
nguyentuyen348 <nguyentuyen348@gmail.com>
2021-10-30 15:45:42 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::get('/', function () {
 });
 Route::get('/register',[RegisterController::class,'showFormRegister'])->name('register');
 Route::post('/register',[RegisterController::class,'register'])->name('auth.register');
+
+Route::prefix('users')->group(function (){
+    Route::get('{id}/profile',[\App\Http\Controllers\UserController::class,'profile'])->name('users.profile');
+    Route::get('{id}/edit',[\App\Http\Controllers\UserController::class,'edit'])->name('users.edit');
+    Route::post('{id}/edit',[\App\Http\Controllers\UserController::class,'update'])->name('users.update');
+});
