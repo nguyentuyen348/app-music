72db6f1338017ad9402ac21da77976e8a55dbe6d
khonghung <ahihimaianh3@gmail.com>
2021-11-03 21:32:44 +0700

    create-migration

--- database/migrations/2021_11_03_091104_create_singer_feedback_table.php
+++ database/migrations/2021_11_03_091104_create_singer_feedback_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSongsTable extends Migration
+class CreateSingerFeedbackTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,13 @@ class CreateSongsTable extends Migration
      */
     public function up()
     {
-        Schema::create('songs', function (Blueprint $table) {
+        Schema::create('singer_feedback', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->string('file_mp3');
-            $table->string('image');
-            $table->string('author');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('category_id');
-            $table->string('album');
-            $table->integer('listens');
+            $table->unsignedBigInteger('singer_id');
+            $table->string('feedback')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('singer_id')->references('id')->on('singers')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -37,6 +31,6 @@ class CreateSongsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('songs');
+        Schema::dropIfExists('singer_feedback');
     }
 }
--- database/migrations/2021_11_03_093439_create_song_comment_table.php
+++ database/migrations/2021_11_03_093439_create_song_comment_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSongsTable extends Migration
+class CreateSongCommentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,19 +13,13 @@ class CreateSongsTable extends Migration
      */
     public function up()
     {
-        Schema::create('songs', function (Blueprint $table) {
+        Schema::create('song_comment', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('description');
-            $table->string('file_mp3');
-            $table->string('image');
-            $table->string('author');
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('category_id');
-            $table->string('album');
-            $table->integer('listens');
+            $table->unsignedBigInteger('song_id');
+            $table->string('comment')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('song_id')->references('id')->on('songs')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -37,6 +31,6 @@ class CreateSongsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('songs');
+        Schema::dropIfExists('song_comment');
     }
 }
