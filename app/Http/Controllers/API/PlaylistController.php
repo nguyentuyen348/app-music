35eb5f4eff7e9c1678535f336157a2958a3d0bb0
khonghung <ahihimaianh3@gmail.com>
2021-11-07 21:34:45 +0700

    Api-playlist

---
+++ app/Http/Controllers/API/PlaylistController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Playlist;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class PlaylistController extends Controller
+{
+    public function store(Request $request){
+        DB::beginTransaction();
+        try {
+            $playlist = new Playlist();
+        $playlist->name = $request->name;
+        $playlist->user_id = $request->user_id;
+        $playlist->category_id = $request->category_id;
+        $playlist->description = $request->description;
+        $playlist->views = $request->views;
+        $playlist->save();
+            DB::commit();
+            $data = [
+                'status' => 'success',
+                'message' => 'Thêm playlist thành công'
+            ];
+            return response()->json($data);
+        } catch (JWTException $exception) {
+            DB::rollBack();
+            $data = [
+                'status' => 'error',
+                'message' => 'Thêm playlist thất bại'
+            ];
+            return response()->json($data);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\API\LoginController;
-use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\API\SongController;
-use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\SongController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\LoginController;
+use App\Http\Controllers\API\PlaylistController;
+use App\Http\Controllers\API\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,7 +34,6 @@ Route::group(['middleware' => ['jwt.verify']], function () {
         Route::post('user', [LoginController::class, 'getAuthenticatedUser']);
         Route::post('me',[LoginController::class,'me']);
         Route::put('change-password', [UserController::class, 'changePassword']);
-
     });
     Route::prefix('songs')->group(function () {
         Route::get('my-songs/{id}',[SongController::class,'getMySongs']);
@@ -41,6 +41,9 @@ Route::group(['middleware' => ['jwt.verify']], function () {
         Route::get('{id}/detailSong', [SongController::class, 'getSongById']);
         Route::put('{id}/update', [SongController::class, 'update']);
         Route::get('{id}/delete',[SongController::class,'delete']);
+    });
+    Route::prefix('playlists')->group(function () {
+        Route::post('create-playlist', [PlaylistController::class, 'store']);
     });
 });
 
